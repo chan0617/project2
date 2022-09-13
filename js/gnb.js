3efbfa2bc62e36fd5b6762090f91bb5a033064dd
chan mi <mo0om487@naver.coml>
2022-09-13 22:56:33 +0900

    분기추가

--- js/gnb.js
+++ js/gnb.js
@@ -1,16 +1,13 @@
 $(function(){
-     //변수선언
     var firstmenu=$('.menu-tit'),
     firstMenuItem=$('.menu-item');
   
-    //아래 this는 변수선택자 2댑스 내려감
     firstmenu.on('mouseenter', function(){
-        firstmenu.find('.dropdown-1').hide(); // 초기화
+        firstmenu.find('.dropdown-1').hide(); 
         firstMenuItem.removeClass('active');
         $(this).find('.menu-item').addClass('active');
         $(this).find('.dropdown-1').slideDown();
     })
-    //2댑스 올라감
     firstmenu.on('mouseleave', function(){
       firstmenu.find('.dropdown-1').hide();
       firstMenuItem.removeClass('active');
@@ -18,7 +15,6 @@ $(function(){
 
 
     var swiper = new Swiper(".mySwiper", {
-      // autoHeight : true,
       loop: true,
       slidesPerView: 1,
       autoplay: {
@@ -36,19 +32,17 @@ $(function(){
       breakpoints: {
         800: {
           slidesPerView: 1,
-          spaceBetween: 20,
+          spaceBetween: 0,
         },
       },
     });
 });
 
-/**
- * $(function(){});
- */
-
 $(document).ready(function(){
   $('#all-menu').click(function(){
+    $(".menu-shadow").toggleClass('active');
     $(this).toggleClass('active');
     $('.gnb-left,.gnb-right').toggleClass('active');
-  })
+  });
 });
+
